db03a3dc1585bf770bb60e15923cd56ff3d92c29
Juan Luis Martínez <juanlu@comakai.com>
2017-02-14 18:55:33 +0100

    Disable bolt and https

--- bootstrap-neo4j-testing.php
+++ bootstrap-neo4j-testing.php
@@ -10,8 +10,9 @@ if (!is_dir(NEO4J_PATH)) {
     exec('wget http://neo4j.com/artifact.php?name=neo4j-community-' . NEO4J_VERSION . '-' . NEO4J_FILE . ' -Ovendor/' . NEO4J_FILE);
     IS_WINDOWS ? exec('unzip vendor/' . NEO4J_FILE . ' -d vendor') : exec('tar xvzf vendor/' . NEO4J_FILE . ' -C vendor');
     exec('rm vendor/' . NEO4J_FILE);
+    exec("sed -i 's/dbms.connector.bolt.enabled=true/dbms.connector.bolt.enabled=false/g' " . NEO4J_PATH . '/conf/neo4j.conf');
     exec("sed -i 's/#dbms.connector.http.address=0.0.0.0:7474/dbms.connector.http.address=0.0.0.0:7475/g' " . NEO4J_PATH . '/conf/neo4j.conf');
-    exec("sed -i 's/dbms.connector.https.address=localhost:7473/#dbms.connector.https.address=localhost:7473/g' " . NEO4J_PATH . '/conf/neo4j.conf');
+    exec("sed -i 's/dbms.connector.https.enabled=true/dbms.connector.https.enabled=false/g' " . NEO4J_PATH . '/conf/neo4j.conf');
     exec('mkdir ' . NEO4J_PATH . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'dbms');
     exec('echo nekuno:SHA-256,F9C128A993975A8C867C996FBAD87CE1E557D9748B5451FF6F5257C19F56BA04,5A9123556B2846896AF655440E3966EC: > ' . NEO4J_PATH . '/data/dbms/auth');
 }
