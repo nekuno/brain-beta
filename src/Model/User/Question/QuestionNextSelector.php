9e28895ca951e8387690429bd12f8cad41e176f4
Roberto Martinez <yawmoght@gmail.com>
2018-02-26 14:32:03 +0100

    QS-1513: Next question logic now filters by question category

--- src/Model/User/Question/QuestionNextSelector.php
+++ src/Model/User/Question/QuestionNextSelector.php
@@ -40,10 +40,18 @@ class QuestionNextSelector
             ->optionalMatch('(user)-[:ANSWERS]->(a:Answer)-[:IS_ANSWER_OF]->(answered:Question)')
             ->optionalMatch('(user)-[:SKIPS]->(skip:Question)')
             ->optionalMatch('(user)-[:REPORTS]->(report:Question)')
-            ->with('user', 'collect(answered) + collect(skip) + collect(report) AS excluded')
-            ->match('(q3:Question)<-[:IS_ANSWER_OF]-(a2:Answer)')
-            ->where('NOT q3 IN excluded', "EXISTS(q3.text_$locale)")
-            ->with('q3 AS question', 'a2')
+            ->with('user', 'collect(answered) + collect(skip) + collect(report) AS excluded');
+
+        $qb->optionalMatch('(user)<-[:PROFILE_OF]-(:Profile)<-[:OPTION_OF]-(mode:Mode)')
+            ->with('user', 'excluded', 'collect(mode) AS userModes', 'count(mode) AS userModeAmount')
+            ->match('(mode:Mode)')
+            ->with('user', 'excluded', 'CASE WHEN userModeAmount > 0 THEN userModes ELSE collect(mode) END AS modes')
+            ->match('(mode)<-[:INCLUDED_IN]-(:QuestionCategory)-[:CATEGORY_OF]->(q:Question)')
+            ->where('mode IN modes');
+
+        $qb->match('(q)<-[:IS_ANSWER_OF]-(a2:Answer)')
+            ->where('NOT q IN excluded', "EXISTS(q.text_$locale)")
+            ->with('q AS question', 'a2')
             ->orderBy('id(a2)')
             ->with('question', 'collect(DISTINCT a2) AS answers')
             ->returns('question', 'answers')
@@ -52,6 +60,7 @@ class QuestionNextSelector
 
         $query = $qb->getQuery();
 
+//        var_dump($query->getExecutableQuery());
         $result = $query->getResultSet();
 
         if (count($result) < 1) {
@@ -83,7 +92,7 @@ class QuestionNextSelector
         $qb = $this->graphManager->createQueryBuilder();
 
         $qb->match('(user:User {qnoow_id: { userId }}), (otherUser:User {qnoow_id: { otherUserId }})')
-            ->match('(otherUser)-[:ANSWERS]->(a:Answer)-[:IS_ANSWER_OF]->(answeredOther:Question)')
+            ->match('(otherUser)-[:ANSWERS]->(otherAnswer:Answer)')
             ->setParameters(
                 array(
                     'userId' => (int)$userId,
@@ -92,11 +101,19 @@ class QuestionNextSelector
             )
             ->optionalMatch('(user)-[:ANSWERS]->(:Answer)-[:IS_ANSWER_OF]->(answered:Question)')
             ->optionalMatch('(user)-[:REPORTS]->(report:Question)')
-            ->with('user', 'a', 'collect(answered) + collect(report) AS excluded')
-            ->match('(q:Question)<-[:IS_ANSWER_OF]-(a)')
-            ->match('(q)<-[:IS_ANSWER_OF]-(allAnswers:Answer)')
-            ->where("NOT q IN excluded", "EXISTS(q.text_$locale)")
-            ->with('q AS question', 'allAnswers')
+            ->with('user', 'otherAnswer', 'collect(answered) + collect(report) AS excluded');
+
+        $qb->optionalMatch('(user)<-[:PROFILE_OF]-(:Profile)<-[:OPTION_OF]-(mode:Mode)')
+            ->with('user', 'excluded', 'collect(mode) AS userModes', 'count(mode) AS userModeAmount')
+            ->match('(mode:Mode)')
+            ->with('user', 'excluded', 'CASE WHEN userModeAmount > 0 THEN userModes ELSE collect(mode) END AS modes')
+            ->match('(mode)<-[:INCLUDED_IN]-(:QuestionCategory)-[:CATEGORY_OF]->(candidateQuestion:Question)')
+            ->where('mode IN modes');
+
+        $qb->match('(candidateQuestion)<-[:IS_ANSWER_OF]-(otherAnswer)')
+            ->match('(candidateQuestion)<-[:IS_ANSWER_OF]-(allAnswers:Answer)')
+            ->where("NOT candidateQuestion IN excluded", "EXISTS(candidateQuestion.text_$locale)")
+            ->with('candidateQuestion AS question', 'allAnswers')
             ->orderBy('id(allAnswers)')
             ->with('question', 'collect(DISTINCT allAnswers) AS answers')
             ->returns('question', 'answers')
@@ -124,10 +141,18 @@ class QuestionNextSelector
         $qb->match('(user:User {qnoow_id: { userId }})')
             ->setParameter('userId', (int)$id)
             ->optionalMatch('(user)-[:ANSWERS]->(a:Answer)-[:IS_ANSWER_OF]->(answered:RegisterQuestion)')
-            ->with('user', 'collect(answered) AS excluded')
-            ->match('(q3:RegisterQuestion)<-[:IS_ANSWER_OF]-(a2:Answer)')
-            ->where('NOT q3 IN excluded', "EXISTS(q3.text_$locale)")
-            ->with('q3 AS question', 'a2')
+            ->with('user', 'collect(answered) AS excluded');
+
+        $qb->optionalMatch('(user)<-[:PROFILE_OF]-(:Profile)<-[:OPTION_OF]-(mode:Mode)')
+            ->with('user', 'excluded', 'collect(mode) AS userModes', 'count(mode) AS userModeAmount')
+            ->match('(mode:Mode)')
+            ->with('user', 'excluded', 'CASE WHEN userModeAmount > 0 THEN userModes ELSE collect(mode) END AS modes')
+            ->match('(mode)<-[:INCLUDED_IN]-(:QuestionCategory)-[:CATEGORY_OF]->(candidateQuestion:RegisterQuestion)')
+            ->where('mode IN modes');
+
+        $qb->match('(candidateQuestion)<-[:IS_ANSWER_OF]-(a2:Answer)')
+            ->where('NOT candidateQuestion IN excluded', "EXISTS(candidateQuestion.text_$locale)")
+            ->with('candidateQuestion AS question', 'a2')
             ->orderBy('id(a2)')
             ->with('question', 'collect(DISTINCT a2) AS answers')
             ->returns('question', 'answers')
