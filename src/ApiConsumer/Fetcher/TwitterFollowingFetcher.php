b0a90c3eb3ce32cd78275750c84fb30fbd027ff5
Roberto Martinez <yawmoght@gmail.com>
2018-04-10 12:09:26 +0200

    QS-1270: Remove unnecessary profile building from fetcher

--- src/ApiConsumer/Fetcher/TwitterFollowingFetcher.php
+++ src/ApiConsumer/Fetcher/TwitterFollowingFetcher.php
@@ -44,37 +44,17 @@ class TwitterFollowingFetcher extends AbstractTwitterFetcher
      */
     protected function parseLinks(array $rawFeed)
     {
-        $lookups = $this->resourceOwner->lookupUsersBy('user_id', $rawFeed, $this->token);
-        $links = $this->resourceOwner->buildProfilesFromLookup($lookups);
-
         $preprocessedLinks = array();
-        if ($links == false || empty($links)) {
-            foreach ($rawFeed as $id) {
-                $link = new Creator();
-                $link->setUrl('https://twitter.com/intent/user?user_id=' . $id);
-                $link->setCreated(1000 * time());
+        foreach ($rawFeed as $id) {
+            $link = new Creator();
+            $link->setUrl('https://twitter.com/intent/user?user_id=' . $id);
+            $link->setCreated(1000 * time());
 
-                $preprocessedLink = new PreprocessedLink($link->getUrl());
-                $preprocessedLink->setFirstLink($link);
-                $preprocessedLink->setSource($this->resourceOwner->getName());
-                $preprocessedLink->setResourceItemId($id);
-                $preprocessedLinks[] = $preprocessedLink;
-            }
-        } else {
-            foreach ($links as &$link) {
-//                $screenName = $link['screen_name'];
-//                $link = $this->resourceOwner->buildProfileFromLookup($link);
-//                $link['processed'] = 1;
-//                $this->resourceOwner->dispatchChannel(
-//                    array(
-//                        'url' => $link['url'],
-//                        'username' => $screenName,
-//                    )
-//                );
-                $preprocessedLink = new PreprocessedLink($link->getUrl());
-                $preprocessedLink->setFirstLink($link);
-                $preprocessedLinks[] = $preprocessedLink;
-            }
+            $preprocessedLink = new PreprocessedLink($link->getUrl());
+            $preprocessedLink->setFirstLink($link);
+            $preprocessedLink->setSource($this->resourceOwner->getName());
+            $preprocessedLink->setResourceItemId($id);
+            $preprocessedLinks[] = $preprocessedLink;
         }
 
         return $preprocessedLinks;
